ee3f2c5cd6ebfbfbce2277978f1a9cc0d5e16527
stevekim72 <steve.kim72@yahoo.com>
2022-03-08 11:14:42 -0800

    added amination to agile intro

--- src/components/agile-coaching/01-agile-coaching-intro.js
+++ src/components/agile-coaching/01-agile-coaching-intro.js
@@ -1,18 +1,28 @@
 import * as React from 'react';
+import { Parallax } from 'react-scroll-parallax';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 const AgileCoachingIntro = () => (
   <div className={`section`}>
-    <h3 className={`section-title text-align-center`}>Agile Coaching</h3>
-    <p>
-      I’ve been an agile practitioner for 15 years, specializing in both Scrum
-      and Kanban. I’ve had the privilege of working with many great teams in
-      many different environments. I’ve even been featured on the Scrum Master
-      Toolbox podcast with Vasco Duarte, author of #NoEstimates - a book that
-      has had an enormously positive impact on my work. These experiences
-      provided me with the opportunity to try new things and apply different
-      approaches that honed my skills &amp; refined the systems and processes I
-      use today.
-    </p>
+    <Parallax speed={5}>
+      <AnimationOnScroll
+        animateIn='animate__fadeIn'
+        animateOut='animate__fadeOut'
+        offset={100}
+      >
+        <h3 className={`section-title text-align-center`}>Agile Coaching</h3>
+        <p>
+          I’ve been an agile practitioner for 15 years, specializing in both
+          Scrum and Kanban. I’ve had the privilege of working with many great
+          teams in many different environments. I’ve even been featured on the
+          Scrum Master Toolbox podcast with Vasco Duarte, author of #NoEstimates
+          - a book that has had an enormously positive impact on my work. These
+          experiences provided me with the opportunity to try new things and
+          apply different approaches that honed my skills &amp; refined the
+          systems and processes I use today.
+        </p>
+      </AnimationOnScroll>
+    </Parallax>
   </div>
 );
 
--- src/components/agile-coaching/02-tools-and-practices.js
+++ src/components/agile-coaching/02-tools-and-practices.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import { Row, Col } from 'react-bootstrap';
 import TabOne from './03-tab1';
 import TabTwo from './04-tab2';
@@ -11,28 +12,45 @@ import * as CoachingStyle from './coaching.module.css';
 
 const ToolsAndPractices = () => (
   <div className={`section ${CoachingStyle.toolsSection}`}>
-    <h4 className={`section-subtitle`}>Tools &amp; Practices</h4>
-    <p>
-      The systems and processes I use better enable agile teams to succeed at
-      delivering high-value, high-quality products for their stakeholders. Some
-      of these systems &amp; processes include:
-    </p>
+    <AnimationOnScroll
+      animateIn='animate__fadeIn'
+      animateOut='animate__fadeOut'
+      offset={100}
+    >
+      <h4 className={`section-subtitle`}>Tools &amp; Practices</h4>
+      <p>
+        The systems and processes I use better enable agile teams to succeed at
+        delivering high-value, high-quality products for their stakeholders.
+        Some of these systems &amp; processes include:
+      </p>
+    </AnimationOnScroll>
     <Row>
+      {/* tab 1 */}
       <Col xl={6} className={`float-shadow white-btn ${CoachingStyle.tab}`}>
         <TabOne />
       </Col>
+
+      {/* tab 2 */}
       <Col xl={6} className={`float-shadow white-btn ${CoachingStyle.tab}`}>
         <TabTwo />
       </Col>
+
+      {/* tab 3 */}
       <Col xl={6} className={`float-shadow white-btn ${CoachingStyle.tab}`}>
         <TabThree />
       </Col>
+
+      {/* tab 4 */}
       <Col xl={6} className={`float-shadow white-btn ${CoachingStyle.tab}`}>
         <TabFour />
       </Col>
+
+      {/* tab 5 */}
       <Col xl={6} className={`float-shadow white-btn ${CoachingStyle.tab}`}>
         <TabFive />
       </Col>
+
+      {/* tab 6 */}
       <Col xl={6} className={`${CoachingStyle.tab} ${CoachingStyle.tabLast}`}>
         <TabSix />
       </Col>
