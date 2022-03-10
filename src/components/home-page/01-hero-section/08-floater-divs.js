c818fe2d1ffd2a8fda51f184df777492681929e2
stevekim72 <steve.kim72@yahoo.com>
2022-03-10 13:45:49 -0800

    className edit

--- src/components/home-page/01-hero-section/08-floater-divs.js
+++ src/components/home-page/01-hero-section/08-floater-divs.js
@@ -8,34 +8,34 @@ const FloaterDivs = () => (
     {/* group 1 */}
     <Parallax speed={-2}>
       <div
-        className={`purple-3d purple-floater05 floater-30 ${HeroStyle.floaterOne}`}
+        className={`purple-3d purple-floater05 floater-30 ${HeroStyle.floater1}`}
       />
     </Parallax>
 
     {/* group 2 */}
     <Parallax speed={-2}>
       <div
-        className={`purple-3d purple-floater1 floater-25 ${HeroStyle.floaterTwo}`}
+        className={`purple-3d purple-floater1 floater-25 ${HeroStyle.floater2}`}
       />
       <div
-        className={`purple-3d purple-floater0 floater-15 ${HeroStyle.floaterThree}`}
+        className={`purple-3d purple-floater0 floater-15 ${HeroStyle.floater3}`}
       />
     </Parallax>
 
     {/* group 3 */}
     <Parallax speed={-4}>
       <div
-        className={`purple-3d purple-floater0 floater-20 ${HeroStyle.floaterFour}`}
+        className={`purple-3d purple-floater0 floater-20 ${HeroStyle.floater4}`}
       />
       <div
-        className={`purple-3d purple-floater1 floater-10 ${HeroStyle.floaterFive}`}
+        className={`purple-3d purple-floater1 floater-10 ${HeroStyle.floater5}`}
       />
     </Parallax>
 
     {/* group 4 */}
     <Parallax speed={-5}>
       <div
-        className={`purple-3d purple-floater05 floater-10 ${HeroStyle.floaterSix}`}
+        className={`purple-3d purple-floater05 floater-10 ${HeroStyle.floater6}`}
       />
     </Parallax>
   </div>
